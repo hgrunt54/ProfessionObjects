d05f2f3dd3ce02e910183816201ffc218d567a33
hgrunt54 <54124819+hgrunt54@users.noreply.github.com>
2019-08-25 19:38:37 -0500

    Add files via upload

---
+++ mod_MainMod.py
@@ -0,0 +1,3 @@
+#! /user/bin/env Python 3.7
+#This is the main module that stores the global variables for Objects such as the following:
+#ProfessionObjectID, ArmorObjectID, WeaponObjectID, SkillsObjectID, and 3 TraitObjectIDs
